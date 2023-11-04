1135bba6587386855c03ce4bfd181da8953b3353
lovepp0518 <lovepp0518@gmail.com>
2023-11-04 18:23:26 +0800

    build: add test data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars') //使用express-handlebars
 const app = express();
 const port = 3000; // 你可以選擇自己的伺服器端口號
+const URLs = require('./public/jsons/URL.json') //使用test data
 
 // 把樣板引擎交給express-handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }))
