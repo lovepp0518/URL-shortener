3f063ac3b254675b645d6f0f07fbc75f675c9703
lovepp0518 <lovepp0518@gmail.com>
2023-11-07 20:32:12 +0800

    feat: generate shorten URL

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { engine } = require('express-handlebars') //使用express-handlebars
 const app = express();
-const port = 3000; // 你可以選擇自己的伺服器端口號
+const port = 3000;
 const URLs = require('./public/jsons/URL.json') //使用test data
+const fs = require('fs'); //使用file system
 
 // 把樣板引擎交給express-handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }))
@@ -18,12 +19,54 @@ app.get('/', (req, res) => {
 });
 
 app.get('/shortener', (req, res) => {
-  res.render('index');
+  let originalURL = req.query.inputOriginalURL?.trim()
+
+  if (originalURL) {
+    // 產生id
+    let id = generateRandomCode(5);
+
+    // 將originalURL, id存入json中
+    let URL = {}
+    URL.originalURLJson = originalURL
+    URL.idJson = id
+    URLs.push(URL);
+
+    // 將物件轉為 JSON （字串）
+    const jsonData = JSON.stringify(URLs);
+
+    // 寫入 JSON 字串到文件
+    fs.writeFile('./public/jsons/URL.json', jsonData, (err) => {
+      if (err)
+        console.log(err);
+      else
+        console.log('Write complete!');
+    });
+
+    // copy要render動態資料
+    res.render('copy', { URL });
+  } else {
+    res.render('index');
+  }
 });
 
+// 產生id：一組5碼亂數
+function generateRandomCode(length) {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let randomCode = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomCode += characters.charAt(randomIndex);
+  }
+
+  return randomCode;
+}
+
+// id要被redirect到originalURL
 app.get('/shortener/:id', (req, res) => {
-  const id = req.params.id
-  res.send(`URL id: ${id}`)
+  let redirectId = req.params.id
+  let redirectURLs = URLs.find((URL) => URL.idJson.toString() === redirectId)
+  res.redirect(redirectURLs.originalURLJson)
 })
 
 // 啟動伺服器
