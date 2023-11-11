be3361279208b77846c9723cabbc0c700dd53c80
lovepp0518 <lovepp0518@gmail.com>
2023-11-11 23:56:16 +0800

    feat: exception handling - same original URL & function writeJsonToFile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars') //使用express-handlebars
 const app = express();
 const port = 3000;
-const URLs = require('./public/jsons/URL.json') //使用test data
+const URLs = require('./public/jsons/URL.json') //使用'./public/jsons/URL.json'
 const fs = require('fs'); //使用file system
 
 // 把樣板引擎交給express-handlebars
@@ -19,39 +19,38 @@ app.get('/', (req, res) => {
 });
 
 app.get('/shortener', (req, res) => {
-  // 例外處理：若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者
-  if (req.query.inputOriginalURL === '') {
-    // 用<script></script>包起來的是在client端執行JavaScript內容：alert彈窗，及重新導向"/shortener"路徑
-    res.send('<script>alert("未輸入內容，請檢查！"); window.location.href = "/shortener";</script>');
-    return
-  }
-
-  let originalURL = req.query.inputOriginalURL?.replace(/\s/g, '');
+  let originalURL = req.query.inputOriginalURL?.trim();
+  let URL = {}
 
   if (originalURL) {
-    // 產生id
-    let id = generateRandomCode(5);
 
-    // 將originalURL, id存入json中
-    let URL = {}
-    URL.originalURLJson = originalURL
-    URL.idJson = id
-    URLs.push(URL);
+    // 例外處理：輸入相同網址時，產生一樣的縮址
+    if (URLs.some((URL) => URL.originalURLJson === originalURL)) {
+      URL = URLs.find((URL) => URL.originalURLJson === originalURL)
+      res.render('copy', { URL }); // copy.hbs要render動態資料
+    }
 
-    // 將物件轉為 JSON （字串）
-    const jsonData = JSON.stringify(URLs);
+    else {
+      // 產生id
+      let id = generateRandomCode(5);
 
-    // 寫入 JSON 字串到文件
-    fs.writeFile('./public/jsons/URL.json', jsonData, (err) => {
-      if (err)
-        console.log(err);
-      else
-        console.log('Write complete!');
-    });
+      // 將originalURL, id存入json中
+      URL.originalURLJson = originalURL
+      URL.idJson = id
+      URLs.push(URL);
 
-    // copy要render動態資料
-    res.render('copy', { URL });
+      writeJsonToFile('./public/jsons/URL.json', URLs);
+
+      res.render('copy', { URL }); // copy.hbs要render動態資料
+    }
   } else {
+    // 例外處理：若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者
+    if (originalURL === '') {
+      // 用<script></script>包起來的是在client端執行JavaScript內容：alert彈窗，及重新導向"/shortener"路徑
+      res.send('<script>alert("未輸入內容，請檢查！"); window.location.href = "/shortener";</script>');
+      return
+    }
+    // originalURL undefined
     res.render('index');
   }
 });
@@ -67,6 +66,21 @@ function generateRandomCode(length) {
   }
 
   return randomCode;
+}
+
+// 將input資料以json檔儲存
+function writeJsonToFile(filePath, data) {
+  // 將物件轉為 JSON 字串
+  const jsonData = JSON.stringify(data);
+
+  // 寫入 JSON 字串到文件
+  fs.writeFile(filePath, jsonData, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Write complete!');
+    }
+  });
 }
 
 // id要被redirect到originalURL
